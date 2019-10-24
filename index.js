49fdf1d3e5f034aec41f13bde48c72b902d622f0
Firstnaruto <firstnaruto@msn.com>
2019-10-24 19:12:58 +0700

    [🐛] Add try catch timeout

--- index.js
+++ index.js
@@ -9,19 +9,23 @@ const puppeteer = require('puppeteer');
   });
   const page = await browser.newPage();
   while (true) {
-    await page.goto('https://regist.ชิมช้อปใช้.com/Register/');
-    const bodyHandle = await page.$('body');
-    const html = await page.evaluate(body => body.innerHTML, bodyHandle);
-    if (
-      html.includes('ขณะนี้คุณอยู่ระหว่างการรอคิวเพื่อลงทะเบียน') ||
-      html.includes('ขณะนี้มีผู้ลงทะเบียนครบ 1 ล้านคนแล้ว')
-    ) {
+    try {
+      await page.goto('https://regist.ชิมช้อปใช้.com/Register/');
+      const bodyHandle = await page.$('body');
+      const html = await page.evaluate(body => body.innerHTML, bodyHandle);
+      if (
+        html.includes('ขณะนี้คุณอยู่ระหว่างการรอคิวเพื่อลงทะเบียน') ||
+        html.includes('ขณะนี้มีผู้ลงทะเบียนครบ 1 ล้านคนแล้ว')
+      ) {
+        continue;
+      } else {
+        setInterval(() => {
+          console.log('Found!!');
+        }, 1000);
+        break;
+      }
+    } catch (e) {
       continue;
-    } else {
-      setInterval(() => {
-        console.log('Found!!');
-      }, 1000);
-      break;
     }
   }
 })();
