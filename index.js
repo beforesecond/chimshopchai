0ac511a9583d215e4aadd92e6b763c26fbe95ec3
Firstnaruto <firstnaruto@msn.com>
2019-11-11 08:57:47 +0700

    [✨] Add 100

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const delay = require('delay');
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -10,12 +11,15 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   while (true) {
     try {
-      await page.goto('https://regist.ชิมช้อปใช้.com/Register/');
+      await page.goto(
+        'https://www.xn--100-mmlpaa9fbc4ccb3l4bd5pe7frde.com/register',
+      );
+      await delay(1000);
       const bodyHandle = await page.$('body');
       const html = await page.evaluate(body => body.innerHTML, bodyHandle);
       if (
         html.includes('ขณะนี้คุณอยู่ระหว่างการรอคิวเพื่อลงทะเบียน') ||
-        html.includes('ขณะนี้มีผู้ลงทะเบียนครบ 1 ล้านคนแล้ว')
+        html.includes('504 Gateway Time-out')
       ) {
         continue;
       } else {
